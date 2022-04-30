f50119bbe915f0cbd7437aace6f312fb46bca225
LeshyDevelopment <leshydevelopment@gmail.com>
2022-04-30 14:53:07 +0200

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3111;
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('Hello, Welcome to the Express starter template for Stackblitz!');
+  res.render('index', {
+    title: 'Hello',
+  });
 });
 
 app.listen(port, () => {
